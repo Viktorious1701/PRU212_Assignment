eb024022035de4037563e4aac7744bc4dc2c38e2
Nguyen Dinh Duong <DuongNDSE183456@fpt.edu.vn>
2025-03-25 21:10:17 +0700

    village awdawdadda

--- Assets/Scripts/DialoguePack/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialoguePack/Scripts/DialogueTrigger.cs
@@ -4,6 +4,14 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogueSystem;
     private bool canInteract = false;
+    [SerializeField] private string[] customMessages = new string[] 
+    {
+        "Take the wheelbarrow to the end of the stage!",
+        "Press 'E' near the wheelbarrow to grab it.",
+        "Good luck on your journey!"
+    };
+    [SerializeField] private string speakerName = "Billboard";
+    [SerializeField] private float messageDuration = 3f;
 
     private void Awake()
     {
@@ -18,6 +26,11 @@ public class DialogueTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             canInteract = true;
+            // Automatically show the message when player enters the trigger area
+            if (dialogueSystem != null && !dialogueSystem.isDialogueActive)
+            {
+                dialogueSystem.Say(customMessages, speakerName, messageDuration);
+            }
         }
     }
 
@@ -33,9 +46,10 @@ public class DialogueTrigger : MonoBehaviour
 
     private void Update()
     {
+        // Optional: Allow player to press E to show the message again while in range
         if (canInteract && dialogueSystem != null && !dialogueSystem.isDialogueActive && Input.GetKeyDown(KeyCode.E))
         {
-            dialogueSystem.Say(new string[] { "Hello, traveler!", "Welcome to our village." }, "Village Girl", 2f);
+            dialogueSystem.Say(customMessages, speakerName, messageDuration);
         }
     }
 }
