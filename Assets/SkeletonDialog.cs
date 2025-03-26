9bcd3e572c514d2f59e78e12741ab6f58dab72f8
Ung Nguyen Song Phuc <songphucungnguyen@gmail.com>
2025-03-27 00:10:56 +0700

    scene 1

---
+++ Assets/SkeletonDialog.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class SkeletonDialog : MonoBehaviour
+{
+    public Dialogue dialogueSystem;
+    private bool canInteract = false;
+    [SerializeField]
+    private string[] customMessages = new string[]
+    {
+        "Beware, adventurer...",
+        "These dungeons are not for the weak...",
+        "Turn back while you still can!"
+    };
+    [SerializeField] private string speakerName = "Skeleton";
+    [SerializeField] private float messageDuration = 3f;
+
+    private void Awake()
+    {
+        if(dialogueSystem == null)
+        {
+            Debug.LogError("dialogueSystem not assigned in DialogueTrigger on " + gameObject.name + ". Please assign it in the Inspector.");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canInteract = true;
+            // Automatically show the message when player enters the trigger area
+            if(dialogueSystem != null && !dialogueSystem.isDialogueActive)
+            {
+                dialogueSystem.Say(customMessages, speakerName, messageDuration);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canInteract = false;
+            if(dialogueSystem != null)
+                dialogueSystem.Clear();
+        }
+    }
+
+    private void Update()
+    {
+        // Optional: Allow player to press E to show the message again while in range
+        if(canInteract && dialogueSystem != null && !dialogueSystem.isDialogueActive && Input.GetKeyDown(KeyCode.E))
+        {
+            dialogueSystem.Say(customMessages, speakerName, messageDuration);
+        }
+    }
+}
