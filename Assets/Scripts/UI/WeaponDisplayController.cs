5ad772b1ebda8785828d2d3f6193695889d83e7a
Thinh An <thinhan1701@gmail.com>
2025-03-26 08:01:14 +0700

    add player combat weapons UI

---
+++ Assets/Scripts/UI/WeaponDisplayController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+using static PlayerCombat;
+
+public class WeaponDisplayController : MonoBehaviour
+{
+    [Header("Weapon Icons")]
+    [SerializeField] private Sprite bareHandIcon;
+    [SerializeField] private Sprite swordIcon;
+    [SerializeField] private Sprite bowIcon;
+    [SerializeField] private Sprite spellIcon;
+
+    [Header("UI References")]
+    [SerializeField] private Image weaponIconImage;
+    [SerializeField] private Text weaponNameText;
+
+    private PlayerCombat playerCombat;
+
+    private void Start()
+    {
+        // Find the PlayerCombat script on the player
+        playerCombat = FindObjectOfType<PlayerCombat>();
+
+        // If you prefer to drag the reference in inspector, you can make this optional
+        if (playerCombat == null)
+        {
+            Debug.LogError("PlayerCombat script not found!");
+            return;
+        }
+
+        // Initial weapon setup
+        UpdateWeaponDisplay();
+    }
+
+    private void Update()
+    {
+        // You'll need to modify the PlayerCombat script to expose the current weapon
+        UpdateWeaponDisplay();
+    }
+
+    private void UpdateWeaponDisplay()
+    {
+        // You'll need to add a method to PlayerCombat to get the current weapon
+        WeaponType currentWeapon = playerCombat.GetCurrentWeapon();
+
+        switch (currentWeapon)
+        {
+            case WeaponType.BareHand:
+                weaponIconImage.sprite = bareHandIcon;
+                weaponNameText.text = "Fists";
+                break;
+            case WeaponType.Sword:
+                weaponIconImage.sprite = swordIcon;
+                weaponNameText.text = "Sword";
+                break;
+            case WeaponType.Bow:
+                weaponIconImage.sprite = bowIcon;
+                weaponNameText.text = "Bow";
+                break;
+            case WeaponType.Spell:
+                weaponIconImage.sprite = spellIcon;
+                weaponNameText.text = "Spell";
+                break;
+        }
+    }
+}
